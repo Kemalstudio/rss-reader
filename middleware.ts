2910c0afd4e5a056ad826470774760346ab73489
Kemal Atayev <kemalatayew913@gmail.com>
2026-05-03 20:58:01 +0500

    1.90

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,10 @@
 import NextAuth from 'next-auth';
-import { authConfig } from './auth.config'; // Импорт из этой же папки
+import { authConfig } from './lib/auth.config'; // Исправленный путь
 
 export default NextAuth(authConfig).auth;
 
 export const config = {
+  // Защищаем все пути, кроме API, статики и картинок
   matcher: [
     '/((?!api|_next/static|_next/image|favicon.ico|preview.jpg).*)',
   ],
